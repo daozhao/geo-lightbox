06b97bf28fc916084d71191cf3cbbdec305df7db
daozhao <daozhao@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-03 08:59:57 +0000

    chang test
    git-svn-id: http://plugins.svn.wordpress.org/geo-lightbox/trunk@977694 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geo-lightbox.php
+++ geo-lightbox.php
@@ -3,15 +3,15 @@
  * Plugin Name: Geo lightbox
  * Plugin URI: 
  * Description: Geo lightbox 
- * Version: 0.5
+ * Version: 0.9
  * Author: daozhao chen 
  * Author URI: http://daozhao.goflytoday.com
  *
- * @copyright 2011
- * @version 0.5
+ * @copyright 2014
+ * @version 0.9
  * @author daozhao chen
  * @link http://daozhao.goflytoday.com/
- * @license 
+ * @license MIT
  *
  */
 
@@ -27,11 +27,8 @@ function wp_daozhao_geo_lightbox_loadjs ()
       
     $path = plugins_url('/geo-lightbox/');
       
-	wp_enqueue_script('google-map-api-v3', 'http://maps.google.com/maps/api/js?sensor=false', array(),"3.0");			
-    //wp_deregister_script('jquery');
-    //wp_enqueue_script('jquery', $path.'jquery-1.3.2.min.js', false, '1.3.2');
-    wp_enqueue_script('geo-lightbox', $path.'jquery.Geolightbox-0.5.js', array('jquery'), '0.5');
-    //wp_enqueue_script('geo-lightbox-plugin', $path.'jquery.geolightbox-wp.js', array('jquery', 'geo-lightbox'), '1.0');
+	//wp_enqueue_script('google-map-api-v3', 'http://maps.google.com/maps/api/js?sensor=false', array(),"3.0");			
+    //wp_enqueue_script('geo-lightbox', $path.'jquery.Geolightbox-0.5.js', array('jquery'), '0.5');
 	wp_enqueue_style('geo-lightbox', $path . 'css/jquery.geolightbox-0.5.css', array(), '1.0');			
       
     
@@ -45,6 +42,11 @@ function wp_daozhao_geo_lightbox_footer()
     if (is_admin() || !function_exists('plugins_url')) return;
       
     $path = plugins_url('/geo-lightbox/');
+      
+//	wp_enqueue_script('google-map-api-v3', 'http://maps.google.com/maps/api/js?sensor=false', array(),"3.0");			
+    wp_enqueue_script('geo-lightbox', $path.'jquery.Geolightbox-0.5.js', array('jquery'), '0.5');
+//	wp_enqueue_style('geo-lightbox', $path . 'css/jquery.geolightbox-0.5.css', array(), '1.0');			
+ 
 	?>
 	<script type="text/javascript">
 		jQuery(document).ready(function(){
@@ -62,7 +64,17 @@ function wp_daozhao_geo_lightbox_footer()
 								imageCurrentCamera:  geo_lightbox_image_path +   'images/camera_geo.png'                               
 							   	}
                                );
+            jQuery.getScript( "http://maps.google.com/maps/api/js?sensor=false&callback=after_load_google" )
+                .done(function( script, textStatus ) {
+                    //console.log( "load google map is ok");
+                })
+                .fail(function( jqxhr, settings, exception ) {
+                    //console.log( "load google map error" );
+                });
 		});
+    function after_load_google(){
+        //console.log( "load google map is done");
+    }
 	</script>
 <?php
     
